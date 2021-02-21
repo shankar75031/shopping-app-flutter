d91c9aa764e6eb48288e0e0c27f9f587e43c3559
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-02-22 01:18:48 +0530

    orders fetching using future builder

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,41 +4,11 @@ import 'package:go_shoppin/widgets/app_drawer.dart';
 import 'package:go_shoppin/widgets/order_item.dart';
 import 'package:provider/provider.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const ROUTE_NAME = '/orders';
 
   @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  // var _isInit = false;
-  // var _isLoading = false;
-
-  @override
-  void initState() {
-    // Future.delayed(Duration.zero).then((_) async {
-    //   setState(() {
-    //     _isLoading = true;
-    //   });
-    //   await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-    //   setState(() {
-    //     _isLoading = false;
-    //   });
-    // });
-
-    // _isLoading = true;
-    // Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_) {
-    //   setState(() {
-    //     _isLoading = false;
-    //   });
-    // });
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final ordersProvider = Provider.of<Orders>(context);
     return Scaffold(
         appBar: AppBar(
           title: Text('Your Orders'),
@@ -58,9 +28,9 @@ class _OrdersScreenState extends State<OrdersScreen> {
               } else {
                 return Consumer<Orders>(
                     builder: (ctx, orderData, child) => ListView.builder(
-                          itemCount: ordersProvider.orders.length,
+                          itemCount: orderData.orders.length,
                           itemBuilder: (ctx, index) =>
-                              OrderItem(ordersProvider.orders[index]),
+                              OrderItem(orderData.orders[index]),
                         ));
               }
             }
