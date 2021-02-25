b71baeb6e707ac57971f4beb0ef58868d0755cdc
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-02-26 03:51:16 +0530

    animated login sign up card and optimized it

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -202,15 +202,15 @@ class _AuthCardState extends State<AuthCard>
 
   void _switchAuthMode() {
     if (_authMode == AuthMode.Login) {
-      _controller.forward();
       setState(() {
         _authMode = AuthMode.Signup;
       });
+      _controller.forward();
     } else {
-      _controller.reverse();
       setState(() {
         _authMode = AuthMode.Login;
       });
+      _controller.reverse();
     }
   }
 
@@ -222,14 +222,17 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        // height: _authMode == AuthMode.Signup ? 320 : 260,
-        height: _heightAnimation.value.height,
-        constraints:
-            // BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
-            BoxConstraints(minHeight: _heightAnimation.value.height),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: (ctx, child) => Container(
+          // height: _authMode == AuthMode.Signup ? 320 : 260,
+          height: _heightAnimation.value.height,
+          constraints:
+              // BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+              BoxConstraints(minHeight: _heightAnimation.value.height),
+          width: deviceSize.width * 0.75,
+          padding: EdgeInsets.all(16.0), child: child,
+        ),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
