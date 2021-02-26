d1db493209741d8d3efc1130841bb63c4c0c4317
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-02-26 16:31:08 +0530

    added hero widget for animating image when opening a page

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -23,7 +23,10 @@ class ProductDetailsScreen extends StatelessWidget {
             Container(
               height: 300,
               width: double.infinity,
-              child: Image.network(product.imageUrl, fit: BoxFit.cover),
+              child: Hero(
+                tag: product.id,
+                child: Image.network(product.imageUrl, fit: BoxFit.cover),
+              ),
             ),
             SizedBox(
               height: 10,
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -28,12 +28,16 @@ class ProductItem extends StatelessWidget {
             Navigator.of(context).pushNamed(ProductDetailsScreen.ROUTE_NAME,
                 arguments: product.id);
           },
-          child: FadeInImage(
-            placeholder: AssetImage('./assets/images/product-placeholder.png'),
-            image: NetworkImage(
-              product.imageUrl,
+          child: Hero(
+            tag: product.id,
+            child: FadeInImage(
+              placeholder:
+                  AssetImage('./assets/images/product-placeholder.png'),
+              image: NetworkImage(
+                product.imageUrl,
+              ),
+              fit: BoxFit.cover,
             ),
-            fit: BoxFit.cover,
           ),
         ),
         footer: GridTileBar(
